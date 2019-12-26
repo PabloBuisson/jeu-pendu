e92f0f9cc46c3d8fcc62639d11d5278cee45abe7
Pablo Buisson <pablo.buisson@gmail.com>
2019-12-26 20:14:04 +0100

    add OOP and click event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Key from './Key.js'
 import Word from './Word.js'
@@ -7,20 +7,27 @@ const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const word = "ELEPHANT"
 
-function App() {
-  return (
-    <div className="game">
-      <div className="word">
-        {word.split("").map( (letter, index) => (
-          <Word letter={letter} key={index} feedback="hidden" />
-        ))}
-      </div>
-      {alphabet.map( (letter) => (
-          <Key letter={letter} key={letter} feedback="visible"/>
+class App extends Component {
+
+  handleLetterClick(letter) {
+    (word.split("").indexOf(letter) !== -1) && console.log(letter, 'gagné');
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="word">
+          {word.split("").map((letter, index) => (
+            <Word letter={letter} key={index} feedback="hidden" />
+          ))}
+        </div>
+        {alphabet.map((letter) => (
+          <Key letter={letter} key={letter} feedback="visible" onClick={this.handleLetterClick} />
         )
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Key.js
+++ src/Key.js
@@ -3,8 +3,8 @@ import './Key.css';
 
 const HIDDEN_SYMBOL = "_"
 
-const Key = ({ letter, feedback }) => (
-    <button className={`key ${feedback}`}>
+const Key = ({ letter, feedback, onClick }) => (
+    <button className={`key ${feedback}`} onClick={() => onClick(letter)}>
         <span className="letter">
             { feedback === 'hidden' ? HIDDEN_SYMBOL : letter }
         </span>
