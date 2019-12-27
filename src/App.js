dbacf7cc5db345bd2cc9ac7341155beb5a816cea
Pablo Buisson <pablo.buisson@gmail.com>
2019-12-27 17:24:20 +0100

    add local state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,45 @@ import './App.css';
 import Key from './Key.js'
 import Word from './Word.js'
 
-const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+const ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const word = "ELEPHANT"
+const WORD = "ELEPHANT"
 
 class App extends Component {
+  state = {
+    word: this.revealWord(WORD, []),
+    usedLetters: [],
+    guesses: 0,
+  }
 
-  handleLetterClick = (letter) => {
-    (word.split("").indexOf(letter) !== -1) && console.log(letter, this);
+  handleLetterClick = letter => {
+    // on récupère les propriétés de l'état local
+    const { guesses, usedLetters } = this.state
+    // on prépare les modifs de l'état local : guesses, letters utilisées
+    const newGuesses = guesses + 1
+    const newUsedLetters = [...usedLetters, ...letter]
+    // on modifie l'état local
+    this.setState({guesses: newGuesses, usedLetters: newUsedLetters})
+    // on verifie si la lettre fait partie du mot + change l'état local
+    const newWord = this.revealWord(WORD, newUsedLetters)
+    this.setState({word: newWord})
+  }
+
+  revealWord(word, usedLetters) { 
+    return word.replace(/\w/g, (letter) => (usedLetters.includes(letter) ? letter : '_')) 
   }
 
   render() {
+    const { word, guesses } = this.state
     return (
-      <div className="game">
-        <div className="word">
+      <div id="game">
+        <div id="guesses">{ guesses }</div>
+        <div id="word">
           {word.split("").map((letter, index) => (
-            <Word letter={letter} key={index} feedback="hidden" />
+            <Word letter={letter} key={index} />
           ))}
         </div>
-        {alphabet.map((letter) => (
+        {ALPHABET.map((letter) => (
           <Key letter={letter} key={letter} feedback="visible" onClick={this.handleLetterClick} />
         )
         )}
--- src/Word.js
+++ src/Word.js
@@ -2,22 +2,18 @@ import React from 'react';
 import './Word.css';
 import PropTypes from 'prop-types'
 
-const HIDDEN_SYMBOL = "_"
+//const HIDDEN_SYMBOL = "_"
 
-const Word = ({ letter, feedback }) => (
-    <div className={`key ${feedback}`}>
+const Word = ({ letter }) => (
+    <div className="key">
         <span className="letter">
-            {feedback === 'hidden' ? HIDDEN_SYMBOL : letter}
+            {letter}
         </span>
     </div>
 )
 
 Word.propTypes = {
     letter: PropTypes.string.isRequired,
-    feedback: PropTypes.oneOf([
-        'hidden',
-        'visible',
-    ]).isRequired,
 }
 
 export default Word;
